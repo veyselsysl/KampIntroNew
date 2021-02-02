81bd9cd9b6354391e45f6a9b0a4dfd155884e298
Asus <Asus@ASUS-BILGISAYAR>
2021-02-02 12:27:13 +0200

    20 Ocak Ders Sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "veysel", "murat", "kerem", "halil" };
+
+            //console.writeline(isimler[0]);
+            //console.writeline(isimler[1]);
+            //console.writeline(isimler[2]);
+            //console.writeline(isimler[3]);
+            //isimler[4] = "ilker";
+            //console.writeline(isimler[4]);
+
+            List<string> isimler2 = new List<string>();
+            isimler2.Add("Veysel");
+            isimler2.Add("2");
+            isimler2.Add("3");
+            isimler2.Add("4");
+            isimler2.Add("5");
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            isimler2.Add("Ali");
+            isimler2.Add("Veli");
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[1]);
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.ProductName = "Masa";
+            product1.CategoryId = 2;
+            product1.UnitPrice = 500;
+            product1.UnitInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitInStock = 5, ProductName = "Kalem", 
+                UnitPrice = 35 };
+
+            //PascalCase   //camelCase
+            //case sensitive
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            //int, double, bool ... değer tip
+            //diziler, class, abstract clas, interface ... referans tip
+
+            //productManager.Topla(4,5);
+            //int tSonucu = productManager.Topla(4,5);
+            //Console.WriteLine(tSonucu);
+        }
+    }
+}
